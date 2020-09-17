5d838695166ee087e7a4283ad63c6473efb4bf73
JinBoH's surface <jinboh@qq.com>
2020-09-17 23:53:06 +0800

    大体模板

---
+++ lesson_1/max_sub.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define Max_lenth 100000
+
+
+int main(int argc, char const *argv[])
+{
+    int lenth,i,j;
+    int num[Max_lenth];
+
+    scanf("%d",&lenth);
+
+    for ( i = 0; i < lenth; i++)
+    {
+        scanf("%d",num+i);
+    }
+    
+    for ( i = 0; i < lenth; i++)
+    {
+        
+    }
+    
+
+    system("pause");
+
+    return 0;
+}
